2687e1f794d61facc5d821e75e4b064639d78661
Lalu Rana Suprayoga <bangjoox77@gmail.com>
2025-12-20 17:53:03 +0800

    feat: register global middleware for activity logging

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Daftarkan middleware LogActivity agar berjalan secara global
+        $middleware->append(\App\Http\Middleware\LogActivity::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
