8283fdbb34c9446c69907decb741a36064dd4fbf
Naraya <adderkf7@gmail.com>
2025-12-24 11:53:53 +0800

    update code app/models/Schedule.php by adji

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -2,11 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Schedule extends Model
 {
-    public function doctor() {
-    return $this->belongsTo(Doctor::class);
+    use HasFactory;
+
+    protected $fillable = [
+        'doctor_id', 
+        'day', 
+        'start_time', 
+        'end_time'
+    ];
+
+    // Relasi ke Dokter
+    public function doctor() 
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    // Agar sistem Appointment Dika bisa melacak jadwal
+    public function appointments() 
+    {
+        return $this->hasMany(Appointment::class);
     }
 }
