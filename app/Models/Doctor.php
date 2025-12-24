cb76cb8c21fe5d474ea378a1e02944a131cbe259
Naraya <Adderkf7@gmail.com>
2025-12-24 11:30:14 +0800

    update APP/MODELS/DOCTOR.php by adji

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -2,11 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
-    public function schedules() {
-    return $this->hasMany(Schedule::class);
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 
+        'specialist', 
+        'phone', 
+        'address'
+    ];
+
+    public function schedules() 
+    {
+        return $this->hasMany(Schedule::class);
+    }
+
+    // Mendukung fitur Booking kerjaan Dika
+    public function appointments() 
+    {
+        return $this->hasMany(Appointment::class);
     }
 }
